4a912a64a609a03b0873bb1bce2601f9da3833ed
winsql <ronjcorpuz11@gmail.com>
2025-09-27 00:23:04 +0800

    added product insights

--- data_analysis.sql
+++ data_analysis.sql
@@ -37,6 +37,8 @@ CREATE TABLE transactions (
 	paymentmethod varchar
 );
 
+-- data are imported
+
 -- table viewing
 SELECT * FROM customers;
 
@@ -44,7 +46,10 @@ SELECT * FROM products;
 
 SELECT * FROM stores;
 
-SELECT * FROM transactions;
+SELECT * FROM transactions
+WHERE customerid in ('C045', 'C170', 'C073');
+
+-- CUSTOMER INSIGHTS
 
 -- Demographic Distribution
 -- gender
@@ -73,18 +78,76 @@ GROUP BY age_group
 ORDER BY num_of_customer DESC;
 
 -- Customer lifetime
-WITH recent_date as(
-	SELECT 
-		customerid, 
-		MAX(date) as recent
-	FROM transactions
-	GROUP BY customerid
-)
 SELECT 
-	c.customerid,
-	c.date_joined,
-	rd.recent as last_transaction,
-	AGE(rd.recent, c.date_joined) as lifetime_days
+    c.customerid,
+    c.date_joined,
+    rd.recent AS last_transaction,
+    AGE(rd.recent, c.date_joined) AS lifetime
 FROM customers c
-JOIN recent_date rd
-ON c.customerid = rd.customerid;
+JOIN (
+    SELECT DISTINCT ON (customerid)
+           customerid,
+           date AS recent
+    FROM transactions
+    ORDER BY customerid, date DESC
+) rd
+  ON c.customerid = rd.customerid
+WHERE AGE(rd.recent, c.date_joined) > interval '0'
+ORDER BY lifetime DESC;
+
+-- Top 3 Customers
+SELECT
+	c.customerid,
+	CONCAT(firstname, ' ', lastname) "name",
+	SUM((p.unitprice * t.quantity) * t.discount) "revenue"
+FROM customers c
+JOIN transactions t
+ON c.customerid = t.customerid
+JOIN products p
+ON t.productid = p.productid
+GROUP BY c.customerid
+ORDER BY revenue DESC
+LIMIT 3;
+
+-- Monthly Sales
+SELECT
+	TO_CHAR(date, 'FMMonth') as month,
+	ROUND(SUM((p.unitprice * t.quantity) * t.discount), 2) as revenue
+FROM transactions t
+JOIN products p
+ON t.productid = p.productid
+GROUP BY month
+ORDER BY revenue DESC;
+
+-- 10 Top-selling products
+SELECT
+	productname,
+	category,
+	SUM(t.quantity) as total_sold
+FROM products p
+JOIN transactions t
+ON p.productid = t.productid
+GROUP BY productname, category
+ORDER BY total_sold DESC
+LIMIT 10;
+
+-- Top Category and sub-category
+SELECT 
+    CASE 
+        WHEN GROUPING(p.category) = 1 THEN 'ALL Categories'
+        ELSE p.category
+    END AS category,
+    CASE 
+        WHEN GROUPING(p.subcategory) = 1 THEN 'ALL Subcategories'
+        ELSE p.subcategory
+    END AS subcategory,
+    SUM(t.quantity) AS total_sold
+FROM products p
+JOIN transactions t 
+    ON p.productid = t.productid
+GROUP BY ROLLUP (p.category, p.subcategory)
+ORDER BY 
+    GROUPING(p.category), 
+    p.category,
+    GROUPING(p.subcategory),
+    p.subcategory;
