36f91bcf048c94199dc562492dcff0cfe19eb3d3
winsql <ronjcorpuz11@gmail.com>
2025-11-12 21:28:57 +0800

    KPIs, cohort

--- data_analysis.sql
+++ data_analysis.sql
@@ -1,6 +1,6 @@
 -- table creation
 
-CREATE TABLE customers (
+CREATE TABLE IF NOT EXISTS customers (
 	CustomerID varchar primary key,
 	firstname varchar,
 	lastname varchar,
@@ -10,7 +10,7 @@ CREATE TABLE customers (
 	date_joined date
 );
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
 	ProductID varchar primary key,
 	productname varchar,
 	category varchar,
@@ -19,14 +19,14 @@ CREATE TABLE products (
 	costprice numeric
 );
 
-CREATE TABLE  stores (
+CREATE TABLE IF NOT EXISTS stores (
 	StoreID varchar primary key,
 	storename varchar,
 	city varchar,
 	region varchar
 );
 
-CREATE TABLE transactions (
+CREATE TABLE IF NOT EXISTS transactions (
 	TransactionID varchar primary key,
 	date date,
 	CustomerID varchar references customers(CustomerID),
@@ -46,8 +46,7 @@ SELECT * FROM products;
 
 SELECT * FROM stores;
 
-SELECT * FROM transactions
-WHERE customerid in ('C045', 'C170', 'C073');
+SELECT * FROM transactions;
 
 -- derived columns
 
@@ -56,110 +55,146 @@ FROM transactions tr
 JOIN products pr
 USING (productid);
 
-SELECT (unitprice - costprice) * quantity as profit
+SELECT (pr.unitprice - pr.costprice) * quantity as profit
 FROM transactions tr
 JOIN products pr
 USING (productid);
 
--- CUSTOMER INSIGHTS
-
--- Demographic Distribution
--- gender
+-- date range
 SELECT
-	gender,
-	COUNT(*) total
-FROM customers
-GROUP BY gender;
+	min(date) start,
+	max(date) end
+FROM transactions;
 
---age
-WITH cte_age AS (
+-- total sales & profit
 SELECT
-	EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM birthdate) as age
-FROM customers)
-SELECT
-	CASE
-		WHEN age BETWEEN 18 AND 25 THEN '18-25'
-		WHEN age BETWEEN 26 AND 35 THEN '26-35'
-		WHEN age BETWEEN 36 AND 50 THEN '36-50'
-		WHEN age BETWEEN 51 AND 64 THEN '51-64'
-		ELSE '65+'
-	END AS age_group,
-	COUNT(*) as num_of_customer
-FROM cte_age
-GROUP BY age_group
-ORDER BY num_of_customer DESC;
-
--- Customer lifetime
-SELECT 
-    c.customerid,
-    c.date_joined,
-    rd.recent AS last_transaction,
-    AGE(rd.recent, c.date_joined) AS lifetime
-FROM customers c
-JOIN (
-    SELECT DISTINCT ON (customerid)
-           customerid,
-           date AS recent
-    FROM transactions
-    ORDER BY customerid, date DESC
-) rd
-  ON c.customerid = rd.customerid
-WHERE AGE(rd.recent, c.date_joined) > interval '0'
-ORDER BY lifetime DESC;
-
--- Top 3 Customers
-SELECT
-	c.customerid,
-	CONCAT(firstname, ' ', lastname) "name",
-	SUM((p.unitprice * t.quantity) * t.discount) "revenue"
-FROM customers c
-JOIN transactions t
-ON c.customerid = t.customerid
-JOIN products p
-ON t.productid = p.productid
-GROUP BY c.customerid
-ORDER BY revenue DESC
-LIMIT 3;
-
--- Monthly Sales
-SELECT
-	TO_CHAR(date, 'FMMonth') as month,
-	ROUND(SUM((p.unitprice * t.quantity) * t.discount), 2) as revenue
+	t.date,
+	p.category,
+	p.subcategory,
+	s.region,
+	ROUND(SUM(t.quantity * (p.unitprice * (1-t.discount))), 2) as salesAmount,
+	ROUND(SUM((p.unitprice - p.costprice) * quantity), 2) as profit
 FROM transactions t
 JOIN products p
-ON t.productid = p.productid
-GROUP BY month
-ORDER BY revenue DESC;
+USING (productid)
+JOIN stores s
+USING (storeid)
+GROUP BY t.date, p.category, p.subcategory, s.region
+ORDER BY t.date;
 
--- 10 Top-selling products
+-- TOP 5 products by total sales
 SELECT
-	productname,
-	category,
-	SUM(t.quantity) as total_sold
-FROM products p
-JOIN transactions t
-ON p.productid = t.productid
-GROUP BY productname, category
-ORDER BY total_sold DESC
-LIMIT 10;
+	p.productname,
+	ROUND(SUM(quantity * (unitprice * (1-discount))), 2) as total_sales
+FROM transactions
+JOIN products p
+USING (productid)
+GROUP BY productname
+ORDER BY total_sales DESC
+LIMIT 5;
 
--- Top Category and sub-category
-SELECT 
-    CASE 
-        WHEN GROUPING(p.category) = 1 THEN 'ALL Categories'
-        ELSE p.category
-    END AS category,
-    CASE 
-        WHEN GROUPING(p.subcategory) = 1 THEN 'ALL Subcategories'
-        ELSE p.subcategory
-    END AS subcategory,
-    SUM(t.quantity) AS total_sold
-FROM products p
-JOIN transactions t 
-    ON p.productid = t.productid
-GROUP BY ROLLUP (p.category, p.subcategory)
-ORDER BY 
-    GROUPING(p.category), 
+-- sales by region
+SELECT
+	s.region,
+	ROUND(SUM(t.quantity * (p.unitprice * (1-t.discount))), 2) as total_sales,
+	ROUND(SUM((p.unitprice - p.costprice) * quantity), 2) as profit
+FROM transactions t
+JOIN products p USING (productid)
+JOIN stores s USING (storeid)
+GROUP BY s.region
+ORDER BY total_sales DESC;
+
+
+-- cleaned fact table
+CREATE MATERIALIZED VIEW IF NOT EXISTS sales_fact AS
+SELECT
+    t.transactionid,
+    t.date,
+    t.customerid,
+    p.productid,
+    s.storeid,
+    s.region,
     p.category,
-    GROUPING(p.subcategory),
-    p.subcategory;
+    p.subcategory,
+    p.productname,
+    p.unitprice,
+    p.costprice,
+    t.quantity,
+    t.discount,
+    ROUND(p.unitprice * t.quantity * (1 - t.discount), 2) AS sales_amount,
+    ROUND((p.unitprice - p.costprice) * t.quantity, 2) AS profit
+FROM transactions t
+JOIN products p ON t.productid = p.productid
+JOIN stores s   ON t.storeid = s.storeid;
+
+REFRESH MATERIALIZED VIEW sales_fact;
+
+-- overall KPIs
+SELECT
+	ROUND(SUM(sales_amount), 2) as total_sales,
+	ROUND(SUM(profit), 2) as total_profit,
+	ROUND(SUM(profit)/SUM(sales_amount)*100,2) as profit_margin_pct,
+	ROUND(AVG(sales_amount), 2) as avg_transaction_value
+FROM sales_fact;
+
+-- monthly sales trend
+SELECT
+	DATE_TRUNC('month', date)::date as month,
+	ROUND(SUM(sales_amount), 2) as total_sales,
+	ROUND(SUM(profit), 2) total_profit,
+	ROUND(SUM(profit)/SUM(sales_amount),2) as profit_margin
+FROM sales_fact
+GROUP BY month
+ORDER BY month;
+
+-- year over year growth
+WITH monthly_sales AS (
+	SELECT
+		DATE_TRUNC('MONTH', date)::date as month,
+		SUM(sales_amount) as total_sales
+	FROM sales_fact
+	GROUP BY month
+)
+SELECT
+	month,
+	total_sales,
+	ROUND(
+		(total_sales - LAG(total_sales) OVER (ORDER BY month))
+		/ NULLIF(LAG(total_sales)OVER (ORDER BY month), 0) * 100, 2
+	) as growth_pct
+FROM monthly_sales
+ORDER BY month;
+
+-- top 5 customers
+SELECT
+	c.customerid,
+	c.firstname || ' ' || c.lastname as customer_name,
+	ROUND(SUM(s.sales_amount), 2) as total_sales,
+	ROUND(SUM(s.profit), 2) as total_profit
+FROM sales_fact s
+JOIN customers c
+USING (customerid)
+GROUP BY c.customerid, customer_name
+ORDER BY total_sales DESC
+LIMIT 5;
+
+-- cohort analysis
+CREATE MATERIALIZED VIEW IF NOT EXISTS customer_first_purchase AS
+SELECT
+	customerid,
+	MIN(DATE_TRUNC('month', date))::date AS first_purchase_month
+FROM sales_fact
+GROUP BY customerid;
+
+CREATE MATERIALIZED VIEW IF NOT EXISTS customer_cohort AS
+SELECT
+	f.customerid,
+	fp.first_purchase_month,
+	DATE_TRUNC('month', f.date)::date as purchase_month,
+	EXTRACT(MONTH FROM AGE(DATE_TRUNC('month', f.date), fp.first_purchase_month)) as months_since_first,
+	SUM(f.sales_amount) as total_sales
+FROM sales_fact f
+JOIN customer_first_purchase fp
+USING (customerid)
+GROUP BY f.customerid, fp.first_purchase_month, purchase_month, months_since_first
+ORDER BY fp.first_purchase_month, months_since_first;
